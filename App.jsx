b3898338462b76f9e3eb2cba37fd792f33a42dfc
CodingwithJosh/ <128726316+jshlrncfblln@users.noreply.github.com>
2024-02-29 18:31:32 +0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import Features from "./components/Features"
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <Features />
+    </div>
+  )
+}
+
+export default App
